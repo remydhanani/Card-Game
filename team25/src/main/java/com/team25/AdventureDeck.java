a1f8eec1947c1ec73bb0f0481e7d3a3584351e9a
farhazvasta <farhaz.vasta@gmail.com>
2018-02-06 00:44:26 -0500

    This is Farhaz committing.
    - Added individual images for each game card

---
+++ team25/src/main/java/com/team25/AdventureDeck.java
@@ -0,0 +1,28 @@
+/*package com.team25;
+import java.util.*;
+
+public class AdventureDeck {
+
+
+	/*
+	 * Assemble cards (create a container for deck) 
+	 * variable = number of cards in deck
+	 * variable2 = number of cards in discard deck
+	 * Shuffle method
+	 * Draw method
+	 * Discard Object (a deck with all the cards that have been discarded)
+	 * 
+	 * (name, location[null], type(weapon?), power)
+	 */
+//	ArrayList<String> deck = new ArrayList<String>();
+//	deck.add("Excalibur", null, "Weapons", "30" );
+//	deck.add("Excalibur", null, "Weapons", "30" );
+//	deck.add("Lance", null, "Weapons", "20" );
+//	deck.add("Lance", null, "Weapons", "20" );
+//	deck.add("Lance", null, "Weapons", "20" );
+//	deck.add("Lance", null, "Weapons", "20" );
+//	deck.add("Lance", null, "Weapons", "20" );
+//	deck.add("Lance", null, "Weapons", "20" );
+	
+
+//}
